eb8ed1fb8fb9aa1de180c85b9cfa7edf22e7a296
Raferto <57932819+Raferto@users.noreply.github.com>
2020-05-13 20:45:05 +0700

    insert JP,PV,K,PL

---
+++ insert - JenisPelanggan & Provinsi & Kota kabupaten.sql
@@ -0,0 +1,77 @@
+/* insert: jenis pelanggan */
+
+insert into JENIS_PELANGGAN values ('JP00001', 'Tenaga Medis', '1');
+insert into JENIS_PELANGGAN values ('JP00002', 'Lembaga Pemerintahan', '2');
+insert into JENIS_PELANGGAN values ('JP00003', 'Toko', '3');
+insert into JENIS_PELANGGAN values ('JP00004', 'Rumah Tangga', '4');
+
+/* insert: Provinsi */
+
+insert into PROVINSI values ('PV00001', 'Jawa Timur');
+insert into PROVINSI values ('PV00002', 'Jawa Tengah');
+insert into PROVINSI values ('PV00003', 'Jawa Barat');
+insert into PROVINSI values ('PV00004', 'Jakarta');
+insert into PROVINSI values ('PV00005', 'Yogyakarta');
+insert into PROVINSI values ('PV00006', 'Sumatera Utara');
+insert into PROVINSI values ('PV00007', 'Sumatera Selatan');
+insert into PROVINSI values ('PV00008', 'Kalimantan Barat');
+insert into PROVINSI values ('PV00009', 'Kalimantan Timur');
+insert into PROVINSI values ('PV00010', 'Sulawesi Selatan');
+insert into PROVINSI values ('PV00011', 'Sulawesi Tenggara');
+insert into PROVINSI values ('PV00012', 'Nusa Tenggara Barat');
+
+/* insert: kota kabupaten */
+
+insert into KOTA_KABUPATEN values ('PV00001','K000001', 'Surabaya');
+insert into KOTA_KABUPATEN values ('PV00001','K000002', 'Malang');
+insert into KOTA_KABUPATEN values ('PV00001','K000003', 'Mojokerto');
+insert into KOTA_KABUPATEN values ('PV00001','K000004', 'Pasuruan');
+
+insert into KOTA_KABUPATEN values ('PV00002','K000005', 'Tegal');
+insert into KOTA_KABUPATEN values ('PV00002','K000006', 'Semarang');
+insert into KOTA_KABUPATEN values ('PV00002','K000007', 'Magelang');
+insert into KOTA_KABUPATEN values ('PV00002','K000008', 'Pekalongan');
+
+insert into KOTA_KABUPATEN values ('PV00003','K000009', 'Bogor');
+insert into KOTA_KABUPATEN values ('PV00003','K000010', 'Depok');
+insert into KOTA_KABUPATEN values ('PV00003','K000011', 'Bekasi');
+insert into KOTA_KABUPATEN values ('PV00003','K000012', 'Cimahi');
+
+insert into KOTA_KABUPATEN values ('PV00004','K000013', 'Jakarta Pusat');
+
+insert into KOTA_KABUPATEN values ('PV00005','K000014', 'Yogyakarta');
+
+insert into KOTA_KABUPATEN values ('PV00006','K000015', 'Medan');
+insert into KOTA_KABUPATEN values ('PV00006','K000016', 'Samosir');
+insert into KOTA_KABUPATEN values ('PV00006','K000017', 'Asahan');
+insert into KOTA_KABUPATEN values ('PV00006','K000018', 'Karo');
+
+insert into KOTA_KABUPATEN values ('PV00007','K000019', 'Lahat');
+insert into KOTA_KABUPATEN values ('PV00007','K000020', 'Banyuasin');
+insert into KOTA_KABUPATEN values ('PV00007','K000021', 'Palembang');
+insert into KOTA_KABUPATEN values ('PV00007','K000022', 'Prabumulih');
+
+insert into KOTA_KABUPATEN values ('PV00008','K000023', 'Pontianak');
+insert into KOTA_KABUPATEN values ('PV00008','K000024', 'Singkawang');
+insert into KOTA_KABUPATEN values ('PV00008','K000025', 'Sintang');
+insert into KOTA_KABUPATEN values ('PV00008','K000026', 'Sambas');
+
+insert into KOTA_KABUPATEN values ('PV00009','K000027', 'Berau');
+insert into KOTA_KABUPATEN values ('PV00009','K000028', 'Balikpapan');
+insert into KOTA_KABUPATEN values ('PV00009','K000029', 'Bontang');
+insert into KOTA_KABUPATEN values ('PV00009','K000030', 'Samarinda');
+
+insert into KOTA_KABUPATEN values ('PV00010','K000031', 'Bantaeng');
+insert into KOTA_KABUPATEN values ('PV00010','K000032', 'Gowa');
+insert into KOTA_KABUPATEN values ('PV00010','K000033', 'Luwu');
+insert into KOTA_KABUPATEN values ('PV00010','K000034', 'Makassar');
+
+insert into KOTA_KABUPATEN values ('PV00011','K000035', 'Bombana');
+insert into KOTA_KABUPATEN values ('PV00011','K000036', 'Kolaka');
+insert into KOTA_KABUPATEN values ('PV00011','K000037', 'Konawe');
+insert into KOTA_KABUPATEN values ('PV00011','K000038', 'Kendari');
+
+insert into KOTA_KABUPATEN values ('PV00012','K000039', 'Bima');
+insert into KOTA_KABUPATEN values ('PV00012','K000040', 'Sumbawa');
+insert into KOTA_KABUPATEN values ('PV00012','K000041', 'Mataram');
+insert into KOTA_KABUPATEN values ('PV00012','K000042', 'Dompu');
